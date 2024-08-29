c27535f7274e07c892e2a5a34626248b53de4553
Grafiel <2381028@unai.edu>
2024-08-29 21:13:35 +0900

    Update TipeDataCharacter.java

--- TipeDataCharacter.java
+++ TipeDataCharacter.java
@@ -3,5 +3,10 @@ public class TipeDataCharacter {
         char u = 'u';
         char n = 'n';
         char a = 'a';
+        char i = 'i';
+        System.out.println(u);
+        System.out.println(n);
+        System.out.println(a);
+        System.out.println(i);
     }
 }
